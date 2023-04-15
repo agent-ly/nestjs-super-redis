fcdd872ad12279791c9145079d18eb34b64c3e7d
ly <developer.lenneth@gmail.com>
2023-04-14 23:49:13 -0700

    Initial

--- src/socket.io/mod.ts
+++ src/socket.io/mod.ts
@@ -1,3 +1,7 @@
+export type {
+  RegisterAdapterOptions as RegisterRedisAdapterOptions,
+  RegisterEmitterOptions as RegisterRedisEmitterOptions,
+} from "./interfaces.js";
 export * from "./common/tokens.js";
 export * from "./common/injectors.js";
 export * from "./module.js";
